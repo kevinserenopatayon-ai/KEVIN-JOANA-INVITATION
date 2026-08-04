64d802f2df493719a48119c23aa6629ba4f1f69c
kevinserenopatayon-ai <kevinserenopatayon@gmail.com>
2026-08-04 19:09:43 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,15 @@
-const observer = new IntersectionObserver(entries => {
-    entries.forEach(entry => {
+const pages = document.querySelectorAll(".page");
+
+const observer = new IntersectionObserver((entries)=>{
+    entries.forEach(entry=>{
         if(entry.isIntersecting){
             entry.target.classList.add("show");
         }
     });
 },{
-    threshold:0.15
+    threshold:0.2
 });
 
-document.querySelectorAll(".reveal").forEach(el=>{
-    observer.observe(el);
+pages.forEach(page=>{
+    observer.observe(page);
 });
